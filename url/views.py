dbe9058ab1835c6b25041d5869e20a7839c830ef
williamjohngardner <bill@williamjohngardner.com>
2016-06-16 13:51:19 -0400

    Site setup and working

--- url/views.py
+++ url/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
+# class LoginView(TemplateView):
+#     template_name = "login.html"
--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from url.views import IndexView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', IndexView.as_view(), name="index"),
+    # url(r'^login$', LoginView.as_view(), name="login")
 ]
