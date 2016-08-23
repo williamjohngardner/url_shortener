0ed07ce15c576f0237db556ae2bd6f13ebe7d161
williamjohngardner <bill@williamjohngardner.com>
2016-08-23 10:11:31 -0400

    cleaning up html formatting

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'url',
-    'whitenoise'
+    'whitenoise',
+    'bootstrapform'
 ]
 
 MIDDLEWARE_CLASSES = [
